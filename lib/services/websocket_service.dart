ba30e537ec73377a2174f38c37709bababf36d8f
Shafi <jonyszone@gmail.com>
2025-03-23 10:00:09 +0600

    web socket service implemented

---
+++ lib/services/websocket_service.dart
@@ -0,0 +1,17 @@
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+class WebSocketService {
+  final WebSocketChannel channel;
+
+  WebSocketService(String url) : channel = WebSocketChannel.connect(Uri.parse(url));
+
+  void sendMessage(String message) {
+    channel.sink.add(message);
+  }
+
+  Stream get stream => channel.stream;
+
+  void dispose() {
+    channel.sink.close();
+  }
+}
